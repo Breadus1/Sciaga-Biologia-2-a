81b204a67ea973106d6cb3c96a590de447940120
Breadus1 <116367179+Breadus1@users.noreply.github.com>
2023-11-15 21:45:13 +0100

    Add files via upload

---
+++ bazadanych.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS tekst (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tresc TEXT
+);
+
+INSERT INTO tekst (tresc) VALUES ('Rodzaje mięśni: Ludzkie ciało zawiera trzy główne typy mięśni:');
+INSERT INTO tekst (tresc) VALUES ('Mięśnie szkieletowe: Przyczepione do kości i odpowiadają za ruchy ciała. Są to mięśnie prążkowane, kontrolowane świadomie.');
+INSERT INTO tekst (tresc) VALUES ('Mięśnie gładkie: Znajdują się w ścianach narządów wewnętrznych, takich jak żołądek, i nie są kontrolowane świadomie.');
+INSERT INTO tekst (tresc) VALUES ('Mięsień sercowy: Specjalny rodzaj mięśnia, który tworzy ścianę serca. Jest prążkowany, ale działa niezależnie od świadomej kontroli.');
+
+INSERT INTO tekst (tresc) VALUES ('Funkcje:');
+INSERT INTO tekst (tresc) VALUES ('Ruch: Mięśnie szkieletowe pracują w parach, aby umożliwić ruch, kurcząc się i rozkurczając.');
+INSERT INTO tekst (tresc) VALUES ('Stabilność i postawa: Pomagają utrzymać równowagę i prawidłową postawę ciała.');
+INSERT INTO tekst (tresc) VALUES ('Ciepłota ciała: Skurcze mięśniowe pomagają generować ciepło, utrzymując temperaturę ciała.');
+
+INSERT INTO tekst (tresc) VALUES ('Struktura i organizacja:');
+INSERT INTO tekst (tresc) VALUES ('Mięśnie są zbudowane z włókien mięśniowych, które mogą się kurczyć i rozkurczać.');
+INSERT INTO tekst (tresc) VALUES ('Włókna mięśniowe są grupowane w pęczki, tworząc większe jednostki funkcjonalne.');
+
+INSERT INTO tekst (tresc) VALUES ('Zdrowie i kondycja:');
+INSERT INTO tekst (tresc) VALUES ('Regularne ćwiczenia i trening siłowy są kluczowe dla utrzymania zdrowia mięśni.');
+INSERT INTO tekst (tresc) VALUES ('Dieta bogata w białko jest ważna dla wzrostu i regeneracji mięśni.');
+
+INSERT INTO tekst (tresc) VALUES ('Choroby i problemy:');
+INSERT INTO tekst (tresc) VALUES ('Mięśnie mogą być dotknięte różnymi schorzeniami, takimi jak dystrofie mięśniowe, skurcze, zmęczenie czy kontuzje.');
+
+INSERT INTO tekst (tresc) VALUES ('Adaptacja i plastyczność:');
+INSERT INTO tekst (tresc) VALUES ('Mięśnie są zdolne do adaptacji, mogą się zwiększać w rozmiarze i siłę w odpowiedzi na regularne ćwiczenia (hipertrofia mięśniowa).');
+INSERT INTO tekst (tresc) VALUES ('Starzenie się wpływa na mięśnie, prowadząc do ich osłabienia i zmniejszenia masy.');
+
+INSERT INTO tekst (tresc) VALUES ('Autor: Marcel Wątruch 2a');
+
